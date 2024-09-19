99587f9e80156c66d91a516fe798600d55c62544
okarpenko <okarpenko@usfca.edu>
2024-09-19 08:52:35 -0700

    Added simple factory and factory method examples.

---
+++ src/main/java/designPatterns/creatingObjects/factoryMethod/naiveBadDesign/PizzaStoreV1TestDrive.java
@@ -0,0 +1,17 @@
+package designPatterns.creatingObjects.factoryMethod.naiveBadDesign;
+
+import designPatterns.creatingObjects.factoryMethod.simplePizzaFactory.Pizza;
+
+public class PizzaStoreV1TestDrive {
+
+	// PizzaStoreV1 is a poor design, since it does not follow the Open/Closed principle
+	public static void main(String[] args) { ;
+		PizzaStoreV1 store = new PizzaStoreV1();
+
+		Pizza pizza = store.orderPizza("cheese");
+		System.out.println("We ordered a " + pizza.getName() + "\n");
+ 
+		pizza = store.orderPizza("veggie");
+		System.out.println("We ordered a " + pizza.getName() + "\n");
+	}
+}
---
+++ src/main/java/designPatterns/creatingObjects/factoryMethod/simplePizzaFactory/PizzaFactory.java
@@ -0,0 +1,5 @@
+package designPatterns.creatingObjects.factoryMethod.simplePizzaFactory;
+
+public interface PizzaFactory {
+    Pizza createPizza(String type);
+}
