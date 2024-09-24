31a3513df8fd2f038d8c83d60a42f4f21efed205
okarpenko <okarpenko@usfca.edu>
2024-09-23 23:54:42 -0700

    Added a simple example of facade pattern.

---
+++ src/main/java/designPatterns/structural/facade/SmartHomeDriver.java
@@ -0,0 +1,14 @@
+package designPatterns.structural.facade;
+
+public class SmartHomeDriver {
+    public static void main(String[] args) {
+        Light light = new Light();
+        Thermostat thermostat = new Thermostat();
+        SecuritySystem securitySystem = new SecuritySystem();
+
+        SmartHomeFacade homeManager = new SmartHomeFacade(light, thermostat, securitySystem);
+        homeManager.arriveHome();
+        System.out.println();
+        homeManager.leaveHome();
+    }
+}
