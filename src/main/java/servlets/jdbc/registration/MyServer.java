567be0f6e8add7414fd2a49a21ad94bcf7d1389c
okarpenko <okarpenko@usfca.edu>
2024-11-19 16:14:14 -0800

    Added a registration example.

---
+++ src/main/java/servlets/jdbc/registration/MyServer.java
@@ -0,0 +1,19 @@
+package servlets.jdbc.registration;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
+public class MyServer {
+	public static final int PORT = 8084;
+
+	public static void main(String[] args) throws Exception {
+		DatabaseHandler dhandler = DatabaseHandler.getInstance();
+		// dhandler.createTable();
+
+		Server server = new Server(PORT);
+		ServletHandler handler = new ServletHandler();
+		handler.addServletWithMapping(RegistrationServlet.class, "/register");
+		server.setHandler(handler);
+		server.start();
+		server.join();
+	}
+}
